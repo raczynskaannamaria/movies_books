c2e0c502c520c3e56f47824d3546a5503aa4c6bd
Anna Raczyńska <raczynska.annamaria@gmail.com>
2023-05-19 22:45:16 +0200

    Create usecases

---
+++ lib/domain/usecases/get_trending_usecase.dart
@@ -0,0 +1,14 @@
+import 'package:dartz/dartz.dart';
+import 'package:movies_books/core/error/failure.dart';
+import 'package:movies_books/data/repository/base_repository_impl.dart';
+import 'package:movies_books/domain/entities/movie_entity.dart';
+
+class GetTrendingUseCase {
+  BaseRepository baseRepository;
+
+  GetTrendingUseCase(this.baseRepository);
+
+  Future<Either<Failure, List<MovieEntity>>> execute() async {
+    return await baseRepository.getTrending();
+  }
+}
