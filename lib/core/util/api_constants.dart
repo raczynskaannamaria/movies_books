17ee58d3376f4986282a536ef6111299a6a2a69e
Anna Raczyńska <raczynska.annamaria@gmail.com>
2023-05-19 21:48:24 +0200

    Add Api constants

---
+++ lib/core/util/api_constants.dart
@@ -0,0 +1,7 @@
+class ApiConstants {
+  ApiConstants._();
+
+  final String base_URL = 'https://api.themoviedb.org/3';
+  final String API_key = '22934fc587492cdae5a07a5d28fa8df2';
+  final String image_url = 'https://image.tmdb.org/t/p/w500';
+}
