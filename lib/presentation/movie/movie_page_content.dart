00f832a14ab1d92a6b691fd786b0006b121c3457
Anna Raczyńska <raczynska.annamaria@gmail.com>
2023-05-22 19:54:01 +0200

    Implement Seach section UI

--- lib/presentation/movie/movie_page_content.dart
+++ lib/presentation/movie/movie_page_content.dart
@@ -1,25 +1,71 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:movies_books/core/resources/app_colors.dart';
+import 'package:movies_books/core/resources/app_text.dart';
+import 'package:movies_books/core/services/service_locator.dart';
 import 'package:movies_books/core/widgets/movies_list.dart';
 import 'package:movies_books/core/widgets/title_section.dart';
+import 'package:movies_books/presentation/movie/bloc/movie_bloc.dart';
 
 class MoviePageContent extends StatelessWidget {
   const MoviePageContent({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: SingleChildScrollView(
-            physics: BouncingScrollPhysics(),
-            child: Column(children: [
-              TitleSection(title: 'TRENDING'),
-              MoviesList(),
-              TitleSection(title: 'LATEST RELEASES'),
-              MoviesList(),
-              TitleSection(title: 'TOP RATED'),
-              MoviesList(),
-              TitleSection(title: 'UPCOMING'),
-              MoviesList(),
-            ])));
+    return BlocProvider(
+        create: (context) => getIt<MovieBloc>()..add(GetTrendingEvent()),
+        child: Scaffold(
+            body: SingleChildScrollView(
+                child: Column(
+          children: [
+            SearchSection(),
+            TitleSection(isLoading: false, title: 'TRENDING'),
+            MoviesList(),
+            TitleSection(isLoading: false, title: 'LATEST RELEASES'),
+            MoviesList(),
+            TitleSection(isLoading: false, title: 'TOP RATED'),
+            MoviesList(),
+            TitleSection(isLoading: false, title: 'UPCOMING'),
+            MoviesList(),
+          ],
+        ))));
+  }
+}
+
+class SearchSection extends StatelessWidget {
+  const SearchSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Texts.text1(string: 'Find a Movie'),
+            const Padding(
+                padding: EdgeInsets.only(top: 25),
+                child: TextField(
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderSide:
+                              BorderSide(width: 0, style: BorderStyle.none),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(15.0),
+                          ),
+                        ),
+                        contentPadding: EdgeInsets.symmetric(vertical: 18),
+                        filled: true,
+                        fillColor: AppColor.alto,
+                        hintText: 'Find a movie',
+                        hintStyle:
+                            TextStyle(color: AppColor.waterloo, fontSize: 14),
+                        prefixIcon: Icon(
+                          Icons.search_outlined,
+                          color: AppColor.cello,
+                        )))),
+          ],
+        ));
   }
 }
